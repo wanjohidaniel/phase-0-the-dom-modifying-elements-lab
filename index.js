0a308969104198ff134fdd0d1eab2b307f88af18
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-28 23:36:38 -0700

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const ul = document.getElementById(main);
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = "YOUR-NAME is the champion";
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
